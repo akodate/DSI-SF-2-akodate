99fce75135d230649fef08f68b4ae564cfd6b572
akodate <alex.kodate@gmail.com>
2016-10-24 15:02:51 -0700

    Post-course update

---
+++ capstones/michael-schulze/readme.md
@@ -0,0 +1,2 @@
+### Iambic_regressor
+
---
+++ curriculum/week-11/1.1-intro-to-R/package_installations.R
@@ -0,0 +1,7 @@
+pkgs = c('data.table','reshape2','plyr','magrittr','dplyr','tidyr','lubridate','ggplot2',
+         'lme4','caret','boot','forecast','lattice','MASS','nnet','quantreg',
+         'zoo','mgcv','gbm','parallel','nlme','lmerTest','ggthemes','psych',
+         'gmodels','corrgram','glmnet','class')
+
+#install.packages(pkgs, dependencies=TRUE)
+install.packages(pkgs, dependencies=FALSE)
